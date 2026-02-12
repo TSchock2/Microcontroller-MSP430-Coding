92d9567a16438577bfe618c15c09ccc336375c89
TSchock2 <tyler.schock02@gmail.com>
2026-02-12 06:36:42 -0500

    Update main.c

--- Timer-Interrupts/main.c
+++ Timer-Interrupts/main.c
@@ -42,28 +42,28 @@ volatile unsigned char ISR_Flag_10 = 0; // Flag to tell main() that a Timer A0 i
 
 void main(void) {
 
-WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__); // Disable watchdog timer
-PMM_unlockLPM5(); // Release all pins on MCU
+WDT_A_hold(__MSP430_BASEADDRESS_WDT_A__);                                             // Disable watchdog timer
+PMM_unlockLPM5();                                                                     // Release all pins on MCU
 
-Init_GPIO(); // Initialize GPIO pins
-Init_Timer_A0(); // Initialize Timer A0
+Init_GPIO();                                                                          // Initialize GPIO pins
+Init_Timer_A0();                                                                      // Initialize Timer A0
 
-P1DIR |= BIT0; // Initialize Bit 0 to pin 1 for Red
-P9DIR |= BIT7; // Initialize Bit 7 to pin 9 for Green
-__enable_interrupt(); // Enable interrupts
+P1DIR |= BIT0;                                                                        // Initialize Bit 0 to pin 1 for Red
+P9DIR |= BIT7;                                                                        // Initialize Bit 7 to pin 9 for Green
+__enable_interrupt();                                                                 // Enable interrupts
 
     while(1)
     {
-        if(ISR_Flag==1) // Timer A0 has occurred.
+        if(ISR_Flag==1)                                                               // Timer A0 has occurred.
         {
             ISR_Flag = 0;
         }
-        if(ISR_Flag_10 ==1) // 1 Sec interval
+        if(ISR_Flag_10 ==1)                                                           // 1 Sec interval
         {
             ISR_Flag_10 = 0;
 
-            P1OUT ^= BIT0; // Toggle bit value for Red LED
-            P9OUT ^= BIT7; // Toggle bit value for Green LED
+            P1OUT ^= BIT0;                                                            // Toggle bit value for Red LED
+            P9OUT ^= BIT7;                                                            // Toggle bit value for Green LED
         }
     }
 
@@ -72,13 +72,13 @@ __enable_interrupt(); // Enable interrupts
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A0(void) {
-        ISR_Flag = 1; // Set ISR_Flag to 1 for Timer A0
-        ISR_Counter++; // Increment ISR_Counter
+        ISR_Flag = 1;                                                                 // Set ISR_Flag to 1 for Timer A0
+        ISR_Counter++;                                                                // Increment ISR_Counter
 
-        if(ISR_Counter >= 10) // Check if ISR_Counter is greater than or equal to 10
+        if(ISR_Counter >= 10)                                                         // Check if ISR_Counter is greater than or equal to 10
         {
-            ISR_Flag_10 = 1; // Set ISR_Flag to 1 for Timer A0 to occur
-            ISR_Counter = 0; // Reset ISR_Counter to 0
+            ISR_Flag_10 = 1;                                                          // Set ISR_Flag to 1 for Timer A0 to occur
+            ISR_Counter = 0;                                                          // Reset ISR_Counter to 0
         }
 
         return;
